3767c05c296a9621f043c20c2a783739181688a0
theeastcoastwest <43519801+theeastcoastwest@users.noreply.github.com>
2020-12-02 15:21:04 -0500

    Update ByteStreamBasics.java
    Updated with appropriate closures for streams.

--- java/ByteStreamBasics.java
+++ java/ByteStreamBasics.java
@@ -20,21 +20,38 @@ public class BasicByteStreams {
     public static void main(String[] args) throws IOException {
 
         // Define I/O streams
-        FileInputStream inStream;
-        FileOutputStream outStream;
+        FileInputStream inStream = null;
+        FileOutputStream outStream = null;
+        
+        // Open file + copy to output
+        try{
+            // Get input stream
+            inStream = new FileInputStream("input.txt");
 
-        // Get input stream
-        inStream = new FileInputStream("input.txt");
+            // Get output Stream
+            outStream = new FileOutputStream("output.txt");
 
-        // Get output Stream
-        outStream = new FileOutputStream("output.txt");
+            // Stream input, write to output
+            int next;  // the next character in the byte stream
+            while((next = inStream.read()) != -1){
 
-        // Stream input, write to output
-        int next;  // the next character in the byte stream
-        while((next = inStream.read()) != -1){
-
-            // Write next character
-            outStream.write(next);
+                // Write next character
+                outStream.write(next);
+            } 
+        }
+        // Ensure that any streams are closed regardless of use/misuse 
+        // to avoid memory leaks.
+        finally {
+            
+            // Close input stream if opened
+            if(inStream != null){
+                inStream.close();
+            }
+            
+            // Close output stream if opened
+            if (outStream != null){
+                inStream.close();
+            }
         }
     }
 }
