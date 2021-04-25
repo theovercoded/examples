85a3a5fa85689e1af5691f01daafa81425ae5db6
theeastcoastwest <43519801+theeastcoastwest@users.noreply.github.com>
2021-04-24 21:37:43 -0400

    Update Singleton.java
    Added a basic example driver method to illustrate usage.
    Added basic attribute w/getter/setter methods to facilitate demonstration of functionality.

--- java/DesignPattern/Singleton.java
+++ java/DesignPattern/Singleton.java
@@ -1,18 +1,38 @@
+package DesignPatterns;
 
 /**
- * A basic implementation of the Singleton pattern such 
- * that only a single instance of the class may be 
+ * A basic implementation of the Singleton pattern such
+ * that only a single instance of the class may be
  * present in the application.
  */
 public class Singleton {
 
-    // A private static method that holds the 
-    // single instance of the Singleton, if 
+    // A private static method that holds the
+    // single instance of the Singleton, if
     // one has been instantiated anywhere.
     private static Singleton _instance = null;
 
+    // A variable to hold a String message
+    String message = "Hello, my name is Singleton.";
+
     /**
-     * Private constructor to prevent instantiation of 
+     * Getter method to return instance message
+     * @return String - Message currently held by Singleton
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Setter method to change the current message
+     * @param message String - value to change the message to.
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    /**
+     * Private constructor to prevent instantiation of
      * Single objects from anywhere other than this Class.
      */
     private Singleton(){}
@@ -28,5 +48,31 @@ public class Singleton {
             _instance = new Singleton();
         }
         return _instance;
+    }
+
+    /**
+     * A simple example driver to illustrate the basic operations of
+     * the Single class.
+     */
+    public static void main(String[] args) {
+
+        // Create a new Singleton object via static method
+        Singleton singleton = Singleton.getInstance();
+
+        // Print the message to standard output
+        System.out.println(singleton.getMessage());
+
+        // Create a new singleton object
+        Singleton singletonTwo = Singleton.getInstance();
+
+        // Create a unique message for the second Singleton
+        singletonTwo.setMessage("Hello, my name is SingletonTwo");
+
+        // Print the message of the second Singleton
+        System.out.println(singletonTwo.getMessage());
+
+        // Print the message of the first Singleton again (it changed!)
+        System.out.println(singleton.getMessage());
+
     }
 }
